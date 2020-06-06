6b22cd02f28d66ce7cf97a03071af20b0ab24164
Alexander Protto <alexander.protto@realpage.com>
2020-06-05 23:50:41 -0500

    Added nullable to datetime property, removed custom modelstate configurarion

--- SocialMedia/SocialMedia.API/Startup.cs
+++ SocialMedia/SocialMedia.API/Startup.cs
@@ -33,7 +33,7 @@ namespace SocialMedia.API
                     }
                 )
                 .ConfigureApiBehaviorOptions(options => {
-                    options.SuppressModelStateInvalidFilter = true;
+                    //options.SuppressModelStateInvalidFilter = true;
                 });
 
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
