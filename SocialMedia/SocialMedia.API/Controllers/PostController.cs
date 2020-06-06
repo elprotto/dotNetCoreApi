b797c3d672c0b92bd3253cacbfe5eb1bc8789116
Alexander Protto <alexander.protto@realpage.com>
2020-06-05 19:19:05 -0500

    Added PostDto, replaced Post Model with PostDto into api controller.

--- SocialMedia/SocialMedia.API/Controllers/PostController.cs
+++ SocialMedia/SocialMedia.API/Controllers/PostController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SocialMedia.Core.DTOs;
 using SocialMedia.Core.Entities;
 using SocialMedia.Core.Interfaces;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace SocialMedia.API.Controllers
@@ -19,21 +21,42 @@ namespace SocialMedia.API.Controllers
         public async Task<IActionResult> GetPosts()
         {
             var posts = await _postRepository.GetPosts();
-
-            return Ok(posts);
+            var postDto = posts.Select(x => new PostDto 
+            { 
+                PostId = x.PostId,
+                Date = x.Date,
+                Description = x.Description,
+                Image = x.Image,
+                UserId = x.UserId
+            });
+            return Ok(postDto);
         }
 
         [HttpGet("{id}")]
         public async Task<IActionResult> GetPost(int id)
         {
             var post = await _postRepository.GetPost(id);
-
-            return Ok(post);
+            var postDto = new PostDto
+            {
+                PostId = post.PostId,
+                Date = post.Date,
+                Description = post.Description,
+                Image = post.Image,
+                UserId = post.UserId
+            };
+            return Ok(postDto);
         }
 
         [HttpPost]
-        public async Task<IActionResult> InsertPost(Post post)
+        public async Task<IActionResult> InsertPost(PostDto postDto)
         {
+            var post = new Post 
+            {
+                Date = postDto.Date,
+                Description = postDto.Description,
+                Image = postDto.Image,
+                UserId = postDto.UserId
+            };
             await _postRepository.InsertPost(post);
             return Ok(post);
         }
---
+++ SocialMedia/SocialMedia.Core/DTOs/UserDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SocialMedia.Core.DTOs
+{
+    public class UserDto
+    {
+        public int UserId { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+        public DateTime Birthdate { get; set; }
+        public string PhoneNumber { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
